19e2221f4c99cf7d0b1afe9b844b0542ed12955c
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-06-14 18:55:42 -0500

    Palabras reservadas y nombres de variables

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,14 @@
+/**
+ * Nombres de archivos
+ * todos con minuscula y separados por un guión, NO caracteres especiales
+ */
+
+let constant = 123;
+
+/**
+ * Estandares en nombres de variables
+ * el estandar es usar camelCase para variables y UperCamelCase para nombres de clases
+ * No pueden EMPEZAR con numeros
+ * Pueden empezar con _ guión bajo
+ * Pueden tener $ simbolo de dolar
+ */
