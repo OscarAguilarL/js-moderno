a568175f3b4492a15b26d7872075b831a29ba289
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-06-20 21:56:57 -0500

    For, for of, for in

---
+++ assets/js/ciclos/for.js
@@ -0,0 +1,16 @@
+const heroes = ['Batman', 'Superman', 'MujerMaravilla', 'Aquaman'];
+
+console.warn('For tradicional');
+for (let i = 0; heroes[i]; i++) {
+  console.log(heroes[i]);
+}
+
+console.warn('For in');
+for (let i in heroes) {
+  console.log(heroes[i]);
+}
+
+console.warn('For of');
+for (let heroe of heroes) {
+  console.log(heroe);
+}
