20301d27f12acc45b69a3bae607c412a55cdb600
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-06-20 21:20:38 -0500

    Switch

---
+++ assets/js/switch.js
@@ -0,0 +1,27 @@
+const dia = 2; // o: domingo
+
+switch (dia) {
+  case 0:
+    console.log('Domingo');
+    break;
+  case 1:
+    console.log('Lunes');
+    break;
+  case 2:
+    console.log('Martes');
+    break;
+  case 3:
+    console.log('Miercoles');
+    break;
+  case 4:
+    console.log('Jueves');
+    break;
+  case 4:
+    console.log('Viernes');
+    break;
+  case 5:
+    console.log('Sabado');
+    break;
+  default:
+    console.log('No es un dia de la semana');
+}
