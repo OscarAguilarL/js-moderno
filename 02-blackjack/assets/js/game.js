2fb7982385057b5a0aa577f9fcceeb25033d5f0e
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-06-21 12:22:53 -0500

    Valor de cada carta

--- 02-blackjack/assets/js/game.js
+++ 02-blackjack/assets/js/game.js
@@ -50,3 +50,22 @@ const pedirCarta = () => {
 };
 
 // pedirCarta();
+
+// const valorCarta = (carta) => {
+//   const valor = carta.substring(0, carta.length - 1);
+//   let puntos = 0;
+//   if (isNaN(valor)) {
+//     puntos = valor === 'A' ? 11 : 10;
+//   } else {
+//     puntos = valor * 1;
+//   }
+
+//   console.log(puntos);
+// };
+
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  return isNaN(valor) ? (valor === 'A' ? 11 : 10) : valor * 1;
+};
+
+console.log(valorCarta(pedirCarta()));
