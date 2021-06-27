458cb77c0c709865e189f111336da39064ab5be9
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-06-27 14:21:28 -0500

    Curso básico de POO con JS de Platzi

---
+++ poo-js/module.mjs
@@ -0,0 +1,9 @@
+import { PlatziClass } from './main.js';
+
+const clase67 = new PlatziClass({
+  name: 'Javascript:Orientado a objetos, basado en prototipos',
+  videoID: 'asdjklj2lqk3jq2lkj34lkjq232lk3j',
+});
+
+clase67.reproducir();
+clase67.pausar();
