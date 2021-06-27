814218b3fafaed2706f6542833610c2d6bbbc6ba
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-06-26 20:53:05 -0500

    Clases en JS

--- poo-js/index.js
+++ poo-js/index.js
@@ -24,3 +24,25 @@ const juanita = new Student('Juanita', 15, [
   'Curso Definitivo de HTML y CSS',
   'Curso Practico de HTML y CSS',
 ]);
+
+class Estudiante {
+  constructor({ name, age, email, cursosAprobados = [] }) {
+    this.name = name;
+    this.age = age;
+    (this.email = email), (this.cursosAprobados = cursosAprobados);
+  }
+
+  aprobarCurso(curso) {
+    this.cursosAprobados.push(curso);
+  }
+}
+
+const oscar = new Student({
+  name: 'Oscar',
+  age: 15,
+  email: 'oscar@mail.com',
+  cursosAprobados: [
+    'Análisis de Negocios para Ciencia de Datos',
+    'Visualización de Datos para Bussisnes Inteligence',
+  ],
+});
