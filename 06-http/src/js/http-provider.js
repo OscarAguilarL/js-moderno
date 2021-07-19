25a781a4711272055b9341552037db1eb705dded
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-07-19 13:16:51 -0500

    Peticiones http

---
+++ 06-http/src/js/http-provider.js
@@ -0,0 +1,18 @@
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+
+const getJoke = async () => {
+  try {
+    const resp = await fetch(jokeUrl);
+    if (!resp.ok) {
+      throw new Error('No se pudo realizar la petición');
+    }
+
+    const { icon_url, id, value } = await resp.json();
+
+    return { icon_url, id, value };
+  } catch (err) {
+    throw err;
+  }
+};
+
+export { getJoke };
