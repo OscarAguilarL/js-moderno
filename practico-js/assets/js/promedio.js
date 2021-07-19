7fc580641de0be6d40c60d640aeafdd5b12dad05
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-07-19 18:42:54 -0500

    Delete other courses

--- practico-js/assets/js/promedio.js
+++
@@ -1,8 +0,0 @@
-const calcularMediaAritmetica = (lista) => {
-  const sumaLista = lista.reduce(
-    (accumulator, currentValue) => accumulator + currentValue
-  );
-  return sumaLista / lista.length;
-};
-
-console.log(calcularMediaAritmetica([100, 200, 300, 500]));
